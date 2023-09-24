9588733695621ec513dbc17d9bb788031a5a9156
afcarazo <carazoa0@gmail.com>
2023-09-24 14:12:42 -0300

    cart logic

--- src/app/pages/home/components/product-box/product-box.component.ts
+++ src/app/pages/home/components/product-box/product-box.component.ts
@@ -1,11 +1,26 @@
-import { Component, Input,OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Product } from 'src/app/models/product.model';
 
 @Component({
   selector: 'app-product-box',
-  templateUrl:'./product-box.component.html'
+  templateUrl: './product-box.component.html'
 })
 export class ProductBoxComponent {
   @Input() fullWidthMode = false;
-  
+
+  product: Product | undefined = {
+    id: 1,
+    title: 'Snickers',
+    price: 150,
+    category: 'shoes',
+    description: "Description",
+    image: "https://via.placeholder.com/150"
+  };
+
+  @Output() addToCart = new EventEmitter();
+
+  onAddToCart(): void {
+    this.addToCart.emit(this.product);
+  }
 
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Product } from 'src/app/models/product.model';
+import { CartService } from 'src/app/services/cart.service';
 
 const ROWS_HEIGHT: {[id:number]:number}  = {1:400, 3: 335, 4:350};
 @Component({
@@ -10,6 +12,10 @@ export class HomeComponent {
   rowHeight = ROWS_HEIGHT[this.cols];
   category: string | undefined;
   
+  constructor(private cartService:CartService) { 
+
+  }
+  
 
   onColumnsCountChange(colsNumber: number): void {
     this.cols = colsNumber;
@@ -17,5 +23,15 @@ export class HomeComponent {
   }
   onShowCategory(newCategory:string) { 
     this.category = newCategory;
+  }
+
+  onAddToCart(product: Product): void { 
+    this.cartService.addToCart({
+      product: product.image,
+      name: product.title,
+      price: product.price,
+      quantity: 1,
+      id: product.id
+    });
   }
 }
